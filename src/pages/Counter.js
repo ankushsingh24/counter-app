18ce1a7a2f723a76a28ee1e4f02ff3c18bd54563
Ankush Singh <ankushsahil45@gmail.com>
2022-08-14 21:29:21 +0530

    added Counter

---
+++ src/pages/Counter.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Output from "../components/Output";
+import Button from "../components/Button";
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const plus = () => setCount(count + 1);
+
+  const minus = () => setCount(count - 1);
+
+  return (
+    <div className="container">
+      <br />
+      <div className="col-md-12 d-flex justify-content-center">
+        <Output message="Counter App" />
+      </div>
+
+      <div className="col-md-12 d-flex justify-content-center">
+        <Button color="primary" callme={plus} label="+" />
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <Button color="danger" callme={minus} label="-" />{" "}
+      </div>
+
+      <div className="col-md-12 d-flex justify-content-center">
+        <Output cnt={count} />
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
